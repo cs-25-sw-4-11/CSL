120144479427156786a307091b2b44b1d5e02bdf
Kresten Laust <krestenlaust@gmail.com>
2025-05-02 09:39:48 +0200

    Duration visitor

--- CSL.Tests/UnitTest1.cs
+++ CSL.Tests/UnitTest1.cs
@@ -1,22 +1,43 @@
 namespace CSL.Tests;
 
-using NUnit.Framework;
 using CSL;
+using NUnit.Framework;
+using Antlr4.Runtime;
+using System;
+using System.IO;
 
 [TestFixture]
-public class TestClass
+public class TestDuration
 {
     [SetUp]
     public void Setup()
     {
-        
+    }
+
+    [TestCase("10 min", 10 * Duration.MinuteFactor)]
+    [TestCase("5 hours", 5 * Duration.MinuteFactor)]
+    [TestCase("2 days", 2 * Duration.DayFactor)]
+    [TestCase("3 weeks", 10 * Duration.WeekFactor)]
+    public void TestLiteralsMinuteParam(string text, int minutes)
+    {
+        var stream = CharStreams.fromString(text);
+
+        var lexer = new CSLLexer(stream);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new CSLParser(tokens);
+
+        var tree = parser.prog();
+
+        // Create and use the visitor
+        var visitor = new DurationVisitor();
+        var result = visitor.Visit(tree);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Minutes, Is.EqualTo(minutes));
     }
 
     [Test]
-    public void Test1()
+    public void TestLiteralMinutesSimple()
     {
-        CSLCustomVisitor visitor = new CSLCustomVisitor();
-        
-        Assert.That("abc", Is.SameAs("abc"), "abc is equal to abc");
     }
 }
--- CSL/CSLCustomVisitor.cs
+++ CSL/CSLCustomVisitor.cs
@@ -1,10 +1,9 @@
+namespace CSL;
+
 using System.ComponentModel.DataAnnotations;
 using Antlr4.Runtime.Misc;
 
 public class CSLCustomVisitor : CSLBaseVisitor<string>
 {
-    public override string VisitAddOp([NotNull] CSLParser.AddOpContext context)
-    {
-        return base.VisitAddOp(context);
-    }
+    
 }
