1b3ec5196533fc8f8d5f3cb415c1af2bc3ae2332
Casper S. <71062789+casstef@users.noreply.github.com>
2025-05-05 13:58:45 +0200

    Apply suggestions from code review
    Co-authored-by: Kresten Laust <krestenlaust@gmail.com>

--- CSL.Tests/ClockUnitTest.cs
+++ CSL.Tests/ClockUnitTest.cs
@@ -26,13 +26,15 @@ public class ClockTests
     [TestCase("-1:30")] // Negative hour
     [TestCase("12:60")] // Minute too large
     [TestCase("12:-5")] // Negative minute
-    [TestCase("a:30")]  // Non-numeric hour
-    [TestCase("12:b")]  // Non-numeric minute
+    [TestCase("aa:30")]  // Non-numeric hour
+    [TestCase("12:bb")]  // Non-numeric minute
     [TestCase("12+30")] // Wrong separator
     [TestCase("50/30")] 
     [TestCase("!12::30")] 
     [TestCase("what the sigma")]
     [TestCase("12-30")]
+    [TestCase(":30")]  // Missing hour
+    [TestCase("12:")]  // Missing minutes
     public void TestInvalidClockLiterals(string input)
     {
         Assert.Throws<InvalidLiteralCompilerException>(() => ClockParser.ParseClock(input));
