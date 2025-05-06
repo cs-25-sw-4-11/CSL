39f8a724374f2d4b5dcd3bd217c228bfd42fac40
casstef <casper_steffensen@yahoo.co.uk>
2025-05-06 13:36:12 +0200

    Change empty string check

--- CSL/SubjectVisitor.cs
+++ CSL/SubjectVisitor.cs
@@ -23,7 +23,7 @@ public class SubjectVisitor : CSLBaseVisitor<Subject>
 
         string text = rawText.Substring(1, rawText.Length - 2);
 
-        if (text is "")
+        if (text == "")
         {
             throw new InvalidLiteralCompilerException($"{nameof(Subject)}: Empty subject is not allowed");
         }
