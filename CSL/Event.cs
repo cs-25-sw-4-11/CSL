47523d92524eef640474296a9ad9821002b663d8
Kresten Laust <krestenlaust@gmail.com>
2025-05-07 13:49:53 +0200

    Convert event to record

--- CSL/Event.cs
+++ CSL/Event.cs
@@ -1,10 +1,8 @@
 namespace CSL;
 
-public readonly struct Event(Subject? subject = null, Date? date = null, Clock? clock = null, Duration? duration = null, Description? description = null)
-{
-    public Subject? Subject { get; } = subject;
-    public Date? Date { get; } = date;
-    public Clock? Clock { get; } = clock;
-    public Duration? Duration { get; } = duration;
-    public Description? Description { get; } = description;
-}
+public record Event(
+    Subject? Subject = null,
+    Date? Date = null,
+    Clock? Clock = null,
+    Duration? Duration = null,
+    Description? Description = null);
--- CSL/EventVisitor.cs
+++ CSL/EventVisitor.cs
@@ -8,34 +8,34 @@ public class EventVisitor : CSLBaseVisitor<Event>
     {
         var subject = new SubjectVisitor().VisitSubject(context);
         
-        return new Event(subject: subject);
+        return new Event(Subject: subject);
     }
 
     public override Event VisitDate(CSLParser.DateContext context)
     {
         var date = new DateVisitor().VisitDate(context);
         
-        return new Event(date: date);
+        return new Event(Date: date);
     }
 
     public override Event VisitClock(CSLParser.ClockContext context)
     {
         var clock = new ClockVisitor().VisitClock(context);
         
-        return new Event(clock: clock);
+        return new Event(Clock: clock);
     }
 
     public override Event VisitDuration(CSLParser.DurationContext context)
     {
         var duration = new DurationVisitor().VisitDuration(context);
         
-        return new Event(duration: duration);
+        return new Event(Duration: duration);
     }
 
     public override Event VisitDescription(CSLParser.DescriptionContext context)
     {
         var description = new DescriptionVisitor().VisitDescription(context);
 
-        return new Event(description: description);
+        return new Event(Description: description);
     }
 }
