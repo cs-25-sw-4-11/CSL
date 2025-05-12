f881f705cca1b06b66583cec6a28d63f2d0b8f62
Kresten Laust <krestenlaust@gmail.com>
2025-05-12 11:44:01 +0200

    Add implicit operator to subject

--- CSL/Subject.cs
+++ CSL/Subject.cs
@@ -5,4 +5,6 @@ public readonly struct Subject(string text)
     public string Text { get; } = text;
 
     public override string ToString() => $"'{Text}'";
+    
+    public static implicit operator Subject(string text) => new (text);
 }
